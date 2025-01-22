594ae3de3c77dff31d516693800c7a07e75689fc
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2025-01-22 15:45:21 -0300

    fix: auth controller

--- lib/app/http/controllers/auth_controller.dart
+++ lib/app/http/controllers/auth_controller.dart
@@ -1,9 +1,57 @@
 import 'package:vania/vania.dart';
 
+import '../../models/user.dart';
+
 class AuthController extends Controller {
   Future<Response> register(Request request) async {
     final data = request.all();
-    return Response.json({'data': data});
+
+    final user = await User().query().create({
+      "name": data['name'],
+      "email": data['email'],
+      "password": data['password']
+    });
+
+    return Response.json({
+      'success': true,
+      'message': "User Registered",
+      'data': user,
+    });
+  }
+
+  Future<Response> login(Request request) async {
+    final data = request.all();
+
+    final user =
+        await User().query().where('email', '=', data['email']).first();
+
+    if (user == null) {
+      return Response.json({
+        'success': false,
+        'message': "User not found",
+      }, 404);
+    }
+
+    if (user['password'] != data['password']) {
+      return Response.json({
+        'success': false,
+        'message': "Password not match",
+      }, 401);
+    }
+
+    final token = await Auth().login(user).createToken(
+          expiresIn: Duration(hours: 24),
+          withRefreshToken: true,
+        );
+
+    return Response.json({
+      'success': true,
+      'message': "Login Success",
+      'data': {
+        'user': user,
+        'token': token,
+      },
+    });
   }
 }
 
